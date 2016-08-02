813eb1ae597f549f8b3f4fd7846d46cf3959ab8d
Thiago <thiagoisaiasbrg@gmail.com>
2016-08-02 14:07:10 -0300

    new Users controller - Signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
