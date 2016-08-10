68a62b7541e2e2e1f61e044e1bef655410c8b893
Thiago <thiagoisaiasbrg@gmail.com>
2016-08-09 23:40:21 -0300

    user params [secure version] and new partial for error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
+  private def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
   def new
     @user = User.new
   end
   def show
     @user = User.find(params[:id])
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #ok
+    else
+      render 'new'
+    end
   end
 end
