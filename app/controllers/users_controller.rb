d3d02dfdd2a7ca3c20aef9b51124969ec6949233
Thiago <thiagoisaiasbrg@gmail.com>
2016-08-11 15:26:03 -0300

    signup routes edited / welcome message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #ok
+      flash[:success] = "Welcome to the Posts App!"
+      redirect_to user_path(@user)
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
