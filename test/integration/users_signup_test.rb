e89962fd3930f76304a545ec61193862b8436fd1
Thiago <thiagoisaiasbrg@gmail.com>
2016-08-14 15:51:33 -0300

    user signup test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,15 +1,25 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-    # test "the truth" do
-    #   assert true
-    # end
+    
+    invalid_params = { params: { user: { name: '', email: '', password: 'asd', password_confirmation: 'akaka' } } }
+    valid_params = { params: { user: { name: 'example', email: 'example@email.com', password: 'pass123', password_confirmation: 'pass123' } } }
 
     test 'invalid signup info' do
         get signup_path
 
         assert_no_difference 'User.count' do
-            post users_path, params: { user: { name: '', email: '', password: 'asd', password_confirmation: 'akaka' } }
+            post users_path, invalid_params
         end
+    end
+
+    test 'valid signup' do
+        get signup_path
+
+        assert_difference 'User.count', 1 do
+            post users_path, valid_params
+        end
+        follow_redirect!
+        assert_template 'users/show'
     end
 end
