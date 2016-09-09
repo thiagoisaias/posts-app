7536f35a73faea7b54901ea4fc1bba87f1b3e401
Thiago <thiagoisaiasbrg@gmail.com>
2016-09-08 22:22:03 -0300

    Session Controller - 'Create' action added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    #render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #Log the user in and redirect to his page
+    else
+      #Create an error message
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
