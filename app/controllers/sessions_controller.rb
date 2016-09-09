87e5ee382d22787a36c45a6052f3b3284cdd61a7
Thiago <thiagoisaiasbrg@gmail.com>
2016-09-08 22:39:04 -0300

    Flash error message for invalid login submission

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       #Log the user in and redirect to his page
     else
       #Create an error message
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
