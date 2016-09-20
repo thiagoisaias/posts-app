8fb82b146ec682d963659aedf9bd14da58ca3cf4
Thiago <thiagoisaiasbrg@gmail.com>
2016-09-20 14:44:39 -0300

    Deleting cookies if a user logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -28,6 +28,7 @@ module SessionsHelper
 
    #Logs out the current user and destroy the session
    def log_out
+      forget(current_user)
       session.delete(:user_id)
       @current_user = nil
    end
@@ -37,5 +38,11 @@ module SessionsHelper
       user.remember
       cookies.permanent.signed[:user_id] = user.id
       cookies.permanent[:remember_token] = user.remember_token
+   end
+
+   def forget(user)
+      user.forget
+      cookies.delete(:user_id)
+      cookies.delete(:remember_token)
    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,10 @@ class User < ApplicationRecord
         update_attribute(:remember_digest, User.digest(remember_token))
     end
 
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
+
     # Returns true if the given token matches the digest.
     def authenticated?(remember_token)
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
