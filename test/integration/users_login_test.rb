4f9825329118e5c02c9ae84e6dfdf6059e676e22
Thiago <thiagoisaiasbrg@gmail.com>
2016-09-24 12:37:56 -0300

    Test Login with/without remember_me checkbox

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,6 +5,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
         @user = users(:example)
     end
 
+    test 'login with remember_me checked' do
+        log_in_as(@user, remember_me: '1')
+        assert_not_nil cookies['remember_token']
+    end
+
+    test 'login with remember_me unchecked' do
+        log_in_as(@user, remember_me: '0')
+        assert_nil cookies['remember_me']
+    end
+
     test 'login with invalid information' do
         get login_path
         assert_template 'sessions/new'
