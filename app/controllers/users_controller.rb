20fd1efbf58c3f98219b545b3a0efecd9cff3782
Thiago Isaias <thiagoisaiasbrg@gmail.com>
2016-10-07 17:15:10 -0300

    Set up User edit/update test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   private def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
@@ -33,6 +35,13 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'edit'
+    end
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
     end
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
-
+# User Controller tests
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get signup_path
-    assert_response :success
+  def setup
+    @user = users(:example)
   end
 
+  test 'should get new' do
+    get :new
+    assert_response :success
+  end
 end
