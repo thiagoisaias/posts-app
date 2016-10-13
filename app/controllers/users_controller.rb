b60b271cd955f4d774f74d55ea646b4c1c3f65f9
Thiago Isaias <thiagoisaiasbrg@gmail.com>
2016-10-13 11:02:17 -0300

    Users index - Main list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :index]
   before_action :correct_user, only: [:edit, :update]
 
-  private def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def index
+    @users = User.all
   end
 
   def new
@@ -50,5 +50,11 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
