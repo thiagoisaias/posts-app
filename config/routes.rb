8d58c8c880a787ee558521a34a0895b07321f67c
Thiago Isaias <thiagoisaiasbrg@gmail.com>
2016-10-15 15:51:08 -0300

    user_activation route / controller and db migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
-
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   resources :users
+  resources :account_activations, only: [:edit]
 end
