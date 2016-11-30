30dc045688e3034df9ae20dbe76fe5a5eedbeaa2
Thiago Isaias <thiagoisaiasbrg@gmail.com>
2016-11-30 15:33:13 -0300

    Microposts initial functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
   before_save :downcase_email
   before_create :create_activation_digest
   has_secure_password
